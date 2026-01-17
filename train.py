9e5fe55f1bb674d5f8c2a230cdc34b0eee1a4bac
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-01-17 20:42:27 +0000

    update train script

---
+++ train.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from lerobot.configs.train import DatasetConfig, TrainPipelineConfig, WandBConfig
+from lerobot.policies.act.configuration_act import ACTConfig
+from lerobot.scripts.lerobot_train import train
+
+HF_USER = "reece-omahoney"
+EXP_NAME = "act-cube-pick"
+DATASET_NAME = "cube-pick-4"
+
+
+def main():
+    cfg = TrainPipelineConfig(
+        dataset=DatasetConfig(repo_id=f"{HF_USER}/{DATASET_NAME}"),
+        policy=ACTConfig(repo_id=f"{HF_USER}/{EXP_NAME}"),
+        output_dir=Path("outputs/train/") / EXP_NAME,
+        job_name=EXP_NAME,
+        wandb=WandBConfig(enable=True),
+    )
+
+    train(cfg)
+
+
+if __name__ == "__main__":
+    main()
--- train.sh
+++
@@ -1,8 +0,0 @@
-lerobot-train \
-  --dataset.repo_id=${HF_USER}/cube-pick-4 \
-  --policy.type=act \
-  --output_dir=outputs/train/act_cube_pick \
-  --job_name=act_cube_pick \
-  --policy.device=mps \
-  --wandb.enable=true \
-  --policy.repo_id=${HF_USER}/my_policy
