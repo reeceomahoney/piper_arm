6049cd8ae5a3e50c3b6ae57f9313da4ec917cc44
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-01-18 11:54:27 +0000

    update train config

--- train.py
+++ train.py
@@ -12,7 +12,12 @@ DATASET_NAME = "cube-pick-4"
 def main():
     cfg = TrainPipelineConfig(
         dataset=DatasetConfig(repo_id=f"{HF_USER}/{DATASET_NAME}"),
-        policy=ACTConfig(repo_id=f"{HF_USER}/{EXP_NAME}"),
+        policy=ACTConfig(
+            repo_id=f"{HF_USER}/{EXP_NAME}",
+            device="cuda:0",
+            chunk_size=40,
+            n_action_steps=40,
+        ),
         output_dir=Path("outputs/train/") / EXP_NAME,
         job_name=EXP_NAME,
         wandb=WandBConfig(enable=True),
