cfc83ff04f338ba82ba87cf6beae28f12e1a4a45
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-01-19 12:53:20 +0000

    update scripts

--- scripts/record.py
+++ scripts/record.py
@@ -10,18 +10,19 @@ In this setup:
 from lerobot.scripts.lerobot_record import DatasetRecordConfig, RecordConfig, record
 
 from piper_arm import PiperConfig, PiperTeleoperatorConfig
+from .train import HF_USER
 
 
 def main():
     cfg = RecordConfig(
         robot=PiperConfig(),
         dataset=DatasetRecordConfig(
-            repo_id="reece-omahoney/piper_arm",
+            repo_id=f"{HF_USER}/piper-arm",
             single_task="Pick up the object",
-            fps=10,
-            episode_time_s=10,
-            reset_time_s=1,
-            num_episodes=10,
+            fps=20,
+            episode_time_s=600,
+            reset_time_s=10,
+            num_episodes=50,
             video=True,
             push_to_hub=False,
         ),
--- scripts/zero.py
+++ scripts/zero.py
@@ -1,3 +1,4 @@
+import time
 from piper_sdk import C_PiperInterface_V2
 
 
@@ -5,6 +6,13 @@ def main():
     piper = C_PiperInterface_V2()
     piper.ConnectPort()
     # piper.MasterSlaveConfig(0xFA, 0, 0, 0)
+
+    while not piper.EnablePiper():
+        time.sleep(0.01)
+
+    piper.ModeCtrl(0x01, 0x01, 30, 0x00)
+    piper.JointCtrl(0, 0, 0, 0, 0, 0)
+    piper.GripperCtrl(0, 1000, 0x01, 0)
 
 
 if __name__ == "__main__":
