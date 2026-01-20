d7523e109ce5fc112aba09b24be21df15bee8edc
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-01-20 12:29:03 +0000

    add scene cam

--- piper_arm/piper_robot.py
+++ piper_arm/piper_robot.py
@@ -20,7 +20,10 @@ class PiperConfig(RobotConfig):
         default_factory=lambda: {
             "wrist": RealSenseCameraConfig(
                 serial_number_or_name="123622270993", fps=30, width=640, height=480
-            )
+            ),
+            "scene": RealSenseCameraConfig(
+                serial_number_or_name="128422270436", fps=30, width=640, height=480
+            ),
         }
     )
     teleop_mode: bool = True
