bf7bc0a774f2ae515639fe8eadcfb97d7e514f0b
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-01-22 14:23:41 +0000

    fix policy loading

--- piper_arm/scripts/eval.py
+++ piper_arm/scripts/eval.py
@@ -1,16 +1,21 @@
+from pathlib import Path
+
 from lerobot.configs.eval import EvalConfig, EvalPipelineConfig
+from lerobot.configs.policies import PreTrainedConfig
 from lerobot.envs.configs import LiberoEnv
-from lerobot.policies import SmolVLAConfig
 from lerobot.scripts.lerobot_eval import eval_main
+
+PRETRAINED_PATH = "HuggingFaceVLA/smolvla_libero"
 
 
 def main():
+    policy = PreTrainedConfig.from_pretrained(PRETRAINED_PATH)
+    policy.pretrained_path = Path(PRETRAINED_PATH)
+
     cfg = EvalPipelineConfig(
-        env=LiberoEnv(task="libero_object"),
+        env=LiberoEnv(task="libero_object", task_ids=[0]),
         eval=EvalConfig(n_episodes=1, batch_size=1),
-        policy=SmolVLAConfig.from_pretrained(
-            pretrained_name_or_path="HuggingFaceVLA/smolvla_libero"
-        ),
+        policy=policy,
     )
 
     eval_main(cfg)
