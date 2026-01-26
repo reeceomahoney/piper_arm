f540982c80865c6f44e211827ffb4273d5401a76
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-01-26 11:13:48 +0000

    update eval script and deps

--- piper_arm/scripts/eval.py
+++ piper_arm/scripts/eval.py
@@ -5,16 +5,17 @@ from lerobot.configs.policies import PreTrainedConfig
 from lerobot.envs.configs import LiberoEnv
 from lerobot.scripts.lerobot_eval import eval_main
 
-PRETRAINED_PATH = "HuggingFaceVLA/smolvla_libero"
+PRETRAINED_PATH = "lerobot/pi05_libero_base"
 
 
 def main():
     policy_cfg = PreTrainedConfig.from_pretrained(PRETRAINED_PATH)
     policy_cfg.pretrained_path = Path(PRETRAINED_PATH)
+    policy_cfg.dtype = "bfloat16"
 
     cfg = EvalPipelineConfig(
-        env=LiberoEnv(task="libero_object", task_ids=[0]),
-        eval=EvalConfig(n_episodes=1, batch_size=1),
+        env=LiberoEnv(task="libero_object"),
+        eval=EvalConfig(n_episodes=20, batch_size=1),
         policy=policy_cfg,
     )
 
