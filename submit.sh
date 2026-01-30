6dfa8512232e44c2d67999b9d1d8f66d529f7a69
Reece O'mahoney <kebl6123@htc-login04.arcus-htc.arc.local>
2026-01-30 13:31:45 +0000

    Add slurm script

---
+++ submit.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4G
+#SBATCH --partition=short
+#SBATCH --gres=gpu:h100:1
+
+echo "Starting at $(date +%H:%M)"
+
+./docker/run_singularity.sh
+
