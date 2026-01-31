352643b5ee977db9fcdb6d17ec6ab8cbdfca6cc0
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-01-31 17:37:42 +0000

    update eval script

--- piper_arm/scripts/eval.py
+++ piper_arm/scripts/eval.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import torch
 from lerobot.configs.eval import EvalConfig, EvalPipelineConfig
 from lerobot.configs.policies import PreTrainedConfig
 from lerobot.envs.configs import AlohaEnv
@@ -8,11 +9,16 @@ from lerobot.scripts.lerobot_eval import eval_main
 
 import piper_arm.policies.configuration_act_resize  # noqa: F401
 
-PRETRAINED_PATH = "reece-omahoney/act-aloha-transfer-cube-finetuned"
+DEVICE_ID = 0
+PRETRAINED_PATH = "reece-omahoney/act-aloha-transfer-cube"
 
-os.environ["MUJOCO_GL"] = "egl"
 
 def main():
+    os.environ["MUJOCO_GL"] = "egl"
+    if torch.cuda.device_count() > 1:
+        os.environ["MUJOCO_EGL_DEVICE_ID"] = "1" if DEVICE_ID == 0 else "0"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(DEVICE_ID)
+
     policy_cfg = PreTrainedConfig.from_pretrained(PRETRAINED_PATH)
     policy_cfg.pretrained_path = Path(PRETRAINED_PATH)
 
