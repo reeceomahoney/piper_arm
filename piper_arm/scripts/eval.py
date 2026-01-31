668bac55906686aba1b8b8f384d1c28a4baed6c5
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-01-31 16:48:04 +0000

    update eval script

--- piper_arm/scripts/eval.py
+++ piper_arm/scripts/eval.py
@@ -1,21 +1,24 @@
+import os
 from pathlib import Path
 
 from lerobot.configs.eval import EvalConfig, EvalPipelineConfig
 from lerobot.configs.policies import PreTrainedConfig
-from lerobot.envs.configs import LiberoEnv
+from lerobot.envs.configs import AlohaEnv
 from lerobot.scripts.lerobot_eval import eval_main
 
-PRETRAINED_PATH = "lerobot/pi05_libero_base"
+import piper_arm.policies.configuration_act_resize  # noqa: F401
 
+PRETRAINED_PATH = "reece-omahoney/act-aloha-transfer-cube-finetuned"
+
+os.environ["MUJOCO_GL"] = "egl"
 
 def main():
     policy_cfg = PreTrainedConfig.from_pretrained(PRETRAINED_PATH)
     policy_cfg.pretrained_path = Path(PRETRAINED_PATH)
-    policy_cfg.dtype = "bfloat16"
 
     cfg = EvalPipelineConfig(
-        env=LiberoEnv(task="libero_object"),
-        eval=EvalConfig(n_episodes=20, batch_size=1),
+        env=AlohaEnv(task="AlohaTransferCube-v0"),
+        eval=EvalConfig(n_episodes=500, batch_size=50),
         policy=policy_cfg,
     )
 
