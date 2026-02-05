6f4252d8c69bfadda693ad8a156697c1028f8caa
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-02-05 11:56:42 +0000

    update slurm script

--- bin/submit.sh
+++ bin/submit.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #SBATCH --nodes=1
-#SBATCH --mem-per-cpu=2G
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem-per-cpu=4G
 #SBATCH --time=12:00:00
 #SBATCH --partition=short
 #SBATCH --gres=gpu:h100:1
