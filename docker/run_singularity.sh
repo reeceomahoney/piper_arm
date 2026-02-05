bcb822e042898e2ebedf4259d1272a8afcfc05ef
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-02-05 13:43:42 +0000

    change cluster cache dir

--- docker/run_singularity.sh
+++ docker/run_singularity.sh
@@ -8,10 +8,16 @@ IMAGE="${SINGULARITY_IMAGE:-docker://reeceomahoney/piper-arm:latest}"
 
 singularity run \
   --nv \
-  --env "TERM=${TERM},PYTHONDONTWRITEBYTECODE=1,WANDB_API_KEY=${WANDB_API_KEY},HF_TOKEN=${HF_TOKEN}" \
+  --env "TERM=${TERM}" \
+  --env "PYTHONDONTWRITEBYTECODE=1" \
+  --env "WANDB_API_KEY=${WANDB_API_KEY}" \
+  --env "HF_TOKEN=${HF_TOKEN}" \
+  --env "UV_CACHE_DIR=/work/.cache/uv" \
+  --env "HF_HOME=/work/.cache/huggingface" \
   --bind "${PROJECT_DIR}/piper_arm:/work/piper_arm" \
   --bind "${PROJECT_DIR}/outputs:/work/outputs" \
   --bind "${PROJECT_DIR}/pyproject.toml:/work/pyproject.toml" \
   --bind "${PROJECT_DIR}/uv.lock:/work/uv.lock" \
+  --bind "${PROJECT_DIR}/.cache:/work/.cache" \
   "$IMAGE" \
   uv run train
