7d6e7323279da29cd0c0ea804ff690aab98c973f
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-03-17 14:07:43 +0000

    add vast setup script

---
+++ vast.sh
@@ -0,0 +1,29 @@
+# bashrc (fill these in)
+cat >> ~/.bashrc << 'EOF'
+export PYTHONDONTWRITEBYTECODE=1
+export HF_TOKEN=""
+export WANDB_API_KEY=""
+EOF
+source ~/.bashrc
+
+# dotfiles
+git clone git@github.com:reeceomahoney/.dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+sudo apt install stow
+stow .
+tmux source-file ~/.config/tmux/tmux.conf
+
+# project
+cd /workspace
+git clone git@github.com:reeceomahoney/piper_arm.git
+cd piper_arm
+
+# mise
+curl https://mise.run | sh
+echo "eval \"\$(/root/.local/bin/mise activate bash)\"" >> ~/.bashrc
+source ~/.bashrc
+cd /workspace/piper_arm
+mise trust
+
+# uv
+uv sync
