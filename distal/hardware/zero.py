8edb853a23c796a801b7daacc5d8cbf24c89c439
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-05-04 13:32:27 +0100

    update zero script

--- distal/hardware/zero.py
+++ distal/hardware/zero.py
@@ -4,18 +4,29 @@ from piper_sdk import C_PiperInterface_V2
 
 
 def main():
-    piper = C_PiperInterface_V2()
-    piper.ConnectPort()
-    # piper.MasterSlaveConfig(0xFA, 0, 0, 0)
+    arms = {
+        "left": C_PiperInterface_V2("can_arm_left"),
+        "right": C_PiperInterface_V2("can_arm_right"),
+    }
 
-    while not piper.EnablePiper():
-        time.sleep(0.01)
+    for arm in arms.values():
+        arm.ConnectPort()
+    time.sleep(0.1)
 
-    piper.ModeCtrl(0x01, 0x01, 30, 0x00)
+    for arm in arms.values():
+        while not arm.EnablePiper():
+            time.sleep(0.01)
+
+    for arm in arms.values():
+        arm.ModeCtrl(0x01, 0x01, 30, 0x00)
+
     for _ in range(5):
-        piper.JointCtrl(0, 0, 0, 0, 0, 0)
+        for arm in arms.values():
+            arm.JointCtrl(0, 0, 0, 0, 0, 0)
         time.sleep(0.5)
-    piper.GripperCtrl(0, 1000, 0x01, 0)
+
+    for arm in arms.values():
+        arm.GripperCtrl(0, 1000, 0x01, 0)
 
 
 if __name__ == "__main__":
